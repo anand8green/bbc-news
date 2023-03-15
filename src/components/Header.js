1fd66a8551aafc6c60c47f4f44768c38f35e06e0
Anand Kumar <247devs@gmail.com>
2023-03-15 08:35:46 +0530

    components basic structure created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            Logo and country list will be here
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/NewsSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewsSection() {
+    return (
+        <div>
+            Country news will be here
+        </div>
+    )
+}
+
+export default NewsSection
