b31a144b7421230a4b9770962f1457aafd56778c
Anand Kumar <247devs@gmail.com>
2023-03-15 09:05:40 +0530

    country list created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+
+    const [country, setCountry] = useState("in")
+
+    const handleCountryChange = (e) => {
+        setCountry(e.target.value)
+    }
+
     return (
-        <div className="header">
-            Logo and country list will be here
-        </div>
+        <header className="container">
+            <div className="wrapper">
+                <div className="content">
+                    <div className="logo">
+                        BBC
+                    </div>
+                    <div className="list">
+                        <label> Choose a country </label>
+                        <select name="countries" onChange={handleCountryChange}>
+                            <option value="in">India</option>
+                            <option value="us">US</option>
+                            <option value="gb">UK</option>
+                            <option value="fr">France</option>
+                            <option value="au">Australia</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
