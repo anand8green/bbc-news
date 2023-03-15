40f553436e3632666c5675510d7afda85fc9cd79
Anand Kumar <247devs@gmail.com>
2023-03-15 10:50:34 +0530

    added a placeholder if there is no image provided

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import imageHolder from '../assets/breaking_news.png'
 
-function NewsItem({ title, description, urlToImage, url }) {
+function NewsItem({ title, description, urlToImage = imageHolder, url }) {
     return (
         <div className="card_item" onClick={() => url}>
             <div className="pic">
                 <img
-                    src={urlToImage}
+                    src={urlToImage || imageHolder}
                     alt=""
                     className='pic'
                 />
